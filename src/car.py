564c2efa5974dd3a84c309d3aa3fc42403a6d224
Nils Kulawiak <nils@kulawiak.de>
2019-11-09 16:17:48 +0100

    erste Schritte

--- src/car.py
+++ src/car.py
@@ -12,21 +12,25 @@ class intellgentDriver:
             acc_coefficient,
             accelleration   ,
             acc_delay       ,
-            follow_time):
+            follow_time     ,
+            street_length   ,
+            surrounding):
        self.opt_distant     = opt_distant     
        self.opt_velocity    = opt_velocity    
        self.acc_coefficient = acc_coefficient 
        self.acc_const       = accelleration   
        self.acc_delay       = acc_delay       
-       self.follow_time     = follow_time     
-
+       self.follow_time     = follow_time  
+       self.street_length   = street_length 
+       self.surrounding     = surrounding
+       
     def velocity(self, cars_position, cars_velocity):
         return cars_velocity
-    def accelaration(self, cars_position, cars_velocity, L):
-        return self.idm(cars_position, cars_velocity, L)
+    def accelaration(self, cars_position, cars_velocity):
+        return self.idm(cars_position, cars_velocity)
     
-    def iidm(self, cars_position, cars_velocity, L):
-        z = self.optimalDistant(cars_velocity) / calcdistant(cars_position, L )
+    def iidm(self, cars_position, cars_velocity):
+        z = self.optimalDistant(cars_velocity) / calcdistant(cars_position, self.street_length)
         result = np.empty(len(z))
         limit_idm = self.acc_const * ( 1 - ( cars_velocity / self.opt_velocity )**self.acc_coefficient)
         result[z >= 1] = self.acc_const * (1-z[z >= 1]**2)
@@ -34,20 +38,20 @@ class intellgentDriver:
 
         return  result
 
-    def idm(self, cars_position, cars_velocity, L):
+    def idm(self, cars_position, cars_velocity):
         return self.acc_const * ( 1 - ( cars_velocity / self.opt_velocity )**self.acc_coefficient 
-                                    - ( self.optimalDistant(cars_velocity) / calcdistant(cars_position, L ))**2 )
+                                    - ( self.optimalDistant(cars_velocity) / calcdistant(cars_position, self.street_length ))**2 )
 
     def optimalDistant(self, cars_velocity):
         positiv = cars_velocity * self.follow_time + cars_velocity * calcVelocityDiff(cars_velocity) / (2 * np.sqrt(self.acc_const * self.acc_delay))
         positiv[positiv < 0] = 0
         return self.opt_distant + positiv
 
-def calcdistant(cars_position,lenght):
-    last_element = cars_position[0] - cars_position[-1]
-    dist = np.append(np.diff(cars_position),last_element)
-    dist[dist  < 0 ] = dist[dist < 0] + lenght
-    return dist
+    def calcdistant(self, cars_position):
+        last_element = cars_position[0] - cars_position[-1]
+        dist = np.append(np.diff(cars_position),last_element)
+        dist[dist  < 0 ] = dist[dist < 0] + lenght
+        return dist
 
 def calcVelocityDiff(cars_velocity):
     last_element = cars_velocity[0] - cars_velocity[-1]
@@ -55,3 +59,20 @@ def calcVelocityDiff(cars_velocity):
     diff = np.abs(diff)
     return diff
 
+class car:
+    def __init__(self, opt_distant,
+            opt_velocity   ,
+            acc_coefficient,
+            accelleration   ,
+            acc_delay       ,
+            follow_time     ,
+            street_length   ,
+            surrounding):
+       self.opt_distant     = opt_distant     
+       self.opt_velocity    = opt_velocity    
+       self.acc_coefficient = acc_coefficient 
+       self.acc_const       = accelleration   
+       self.acc_delay       = acc_delay       
+       self.follow_time     = follow_time  
+       self.street_length   = street_length 
+       self.surrounding     = surrounding
--- src/traffic.py
+++ src/traffic.py
@@ -10,22 +10,28 @@ import numerics as nums
 
 # Parameters 
 # System
-num_of_cars     = 10
-street_length   = 1000
-traffic = np.empty(num_of_cars)
+num_of_cars = np.array([3,4,3])
+street_length = 1000
+lanes = 3
 h = 0.1
 
 def main():
-    c = car.intellgentDriver(2, 120 / 3.6, 4, 1, 1.5, 1)
-    traffic_position = np.linspace(100,street_length -500,num_of_cars)
-    traffic_velocity = np.zeros(num_of_cars) + 120 / 3.6
+    c = car.intellgentDriver(2, 120 / 3.6, 4, 1, 1.5, 1, street_length)
+    traffic_position = np.array([np.linspace(100,street_length -500,num_of_cars[0]),
+                                 np.linspace(100,street_length -500,num_of_cars[1]),
+                                 np.linspace(100,street_length -500,num_of_cars[2])])
+    traffic_velocity = []
+    for i in range(lanes):
+        traffic_velocity.append(np.zeros(num_of_cars[i]) + 120 / 3.6)
+    traffic_velocity = np.array(traffic_velocity)
+    
     traffic = np.array([traffic_position,traffic_velocity])
     time_steps = 10000;
     plt.ion()
     plt.show()
     visualization(traffic[0])
     for i in range(time_steps):
-        traffic = nums.rk4(h, traffic,[ c.velocity,c.accelaration],[[],[street_length]])
+        traffic = nums.rk4(h, traffic,[ c.velocity,c.accelaration],[[],[]])
         traffic[0] = traffic[0] % street_length;
         if i%3 == 0:
             visualization(traffic[0])
@@ -38,5 +44,46 @@ def visualization(pos):
     plt.draw()
     plt.pause(1e-6)
     
-
+def calcSurrounding(traffic_position):
+    surrounding = []
+    for i in range(traffic_position.shape[0]):
+        surrounding.append([])
+        for k in range(len(traffic_position[i])):
+             surrounding[i].append(np.zeros(7))
+             surrounding[i][k][0] = k
+             surrounding[i][k][1] = (k+1) % len(traffic_position[i])
+             surrounding[i][k][2] = (k-1) % len(traffic_position[i])
+             
+             if i == 0:
+                surrounding[i][k][3] = -1
+                surrounding[i][k][4] = -1             
+             else:
+                 Abstand = traffic_position[i][k] - traffic_position[i-1][0]
+                 for l in range(len(traffic_position[i-1])): 
+                     if traffic_position[i][k] - traffic_position[i-1][l] < 0 and traffic_position[i][k] - traffic_position[i-1][l] >= Abstand:
+                         Abstand = traffic_position[i][k] - traffic_position[i-1][l]
+                         front = l
+                         back = l-1
+                     else:
+                         front = 0
+                         back = len(traffic_position[i-1])
+                 surrounding[i][k][3] = front
+                 surrounding[i][k][4] = back
+                 
+             if i == traffic_position.shape[0] - 1:
+                surrounding[i][k][5] = -1
+                surrounding[i][k][6] = -1             
+             else:
+                 Abstand = traffic_position[i][k] - traffic_position[i+1][0]
+                 for l in range(len(traffic_position[i+1])): 
+                     if traffic_position[i][k] - traffic_position[i+1][l] < 0 and traffic_position[i][k] - traffic_position[i+1][l] >= Abstand:
+                         Abstand = traffic_position[i][k] - traffic_position[i+1][l]
+                         front = l
+                         back = l-1
+                     else:
+                         front = 0
+                         back = len(traffic_position[i-1])
+                 surrounding[i][k][5] = front
+                 surrounding[i][k][6] = back
+    return surrounding
 main()
